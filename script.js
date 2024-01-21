d2ed50507106e346b4fd5bf1264ecc57d7b84982
lucassfontes <80824656+lucassfontes@users.noreply.github.com>
2024-01-21 18:50:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,466 @@
+let vl_tarifa = document.getElementById('vl_tarifa')
+    
+vl_tarifa.value = '0.00'
+
+let date = new Date()
+let date_hj =  date.getDate()
+
+let dt_1 = document.getElementById('dt_1')
+let dt_2 = document.getElementById('dt_2')
+let dt_3 = document.getElementById('dt_3')
+let dt_4 = document.getElementById('dt_4')
+let dt_5 = document.getElementById('dt_5')
+let dt_6 = document.getElementById('dt_6')
+let dt_7 = document.getElementById('dt_7')
+let dt_8 = document.getElementById('dt_8')
+let dt_9 = document.getElementById('dt_9')
+let dt_10 = document.getElementById('dt_10')
+let dt_11 = document.getElementById('dt_11')
+let dt_12 = document.getElementById('dt_12')
+let dt_13 = document.getElementById('dt_13')
+let dt_14 = document.getElementById('dt_14')
+let dt_15 = document.getElementById('dt_15')
+let dt_16 = document.getElementById('dt_16')
+let dt_17 = document.getElementById('dt_17')
+let dt_18 = document.getElementById('dt_18')
+let dt_19 = document.getElementById('dt_19')
+let dt_20 = document.getElementById('dt_20')
+let dt_21 = document.getElementById('dt_21')
+let dt_22 = document.getElementById('dt_22')
+let dt_23 = document.getElementById('dt_23')
+let dt_24 = document.getElementById('dt_24')
+let dt_25 = document.getElementById('dt_25')
+let dt_26 = document.getElementById('dt_26')
+let dt_27 = document.getElementById('dt_27')
+let dt_28 = document.getElementById('dt_28')
+let dt_29 = document.getElementById('dt_29')
+let dt_30 = document.getElementById('dt_30')
+let dt_31 = document.getElementById('dt_31')
+
+let d_1 = 1
+let d_2 = 2
+let d_3 = 3
+let d_4 = 4
+let d_5 = 5
+let d_6 = 6
+let d_7 = 7
+let d_8 = 8
+let d_9 = 9
+let d_10 = 10
+let d_11 = 11
+let d_12 = 12
+let d_13 = 13
+let d_14 = 14
+let d_15 = 15
+let d_16 = 16
+let d_17 = 17
+let d_18 = 18
+let d_19 = 19
+let d_20 = 20
+let d_21 = 21
+let d_22 = 22
+let d_23 = 23
+let d_24 = 24
+let d_25 = 25
+let d_26 = 26
+let d_27 = 27
+let d_28 = 28
+let d_29 = 29
+let d_30 = 30
+let d_31 = 31
+
+dt_1.value = 1
+dt_1.innerHTML = '1<Br>R$ ' + dt_1.value
+
+dt_2.value = 2
+dt_2.innerHTML = '2<Br>R$ '+ dt_2.value
+
+dt_3.value = 3
+dt_3.innerHTML = '3<Br>R$ '+ dt_3.value
+
+dt_4.value = 4
+dt_4.innerHTML = '4<Br>R$ '+ dt_4.value
+
+dt_5.value = 5
+dt_5.innerHTML = '5<Br>R$ '+ dt_5.value
+
+dt_6.value = 6
+dt_6.innerHTML = '6<Br>R$ '+ dt_6.value
+
+dt_7.value = 7
+dt_7.innerHTML = '7<Br>R$ '+ dt_7.value
+
+dt_8.value = 8
+dt_8.innerHTML = '8<Br>R$ '+ dt_8.value
+
+dt_9.value = 9
+dt_9.innerHTML = '9<Br>R$ '+ dt_9.value
+
+dt_10.value = 10
+dt_10.innerHTML = '10<Br>R$ '+ dt_10.value
+
+dt_11.value = 11
+dt_11.innerHTML = '11<Br>R$ '+ dt_11.value
+
+dt_12.value = 12
+dt_12.innerHTML = '12<Br>R$ '+ dt_12.value
+
+dt_13.value = 13
+dt_13.innerHTML = '13<Br>R$ '+ dt_13.value
+
+dt_14.value = 14
+dt_14.innerHTML = '14<Br>R$ '+ dt_14.value
+
+dt_15.value = 15
+dt_15.innerHTML = '15<Br>R$ '+ dt_15.value
+
+dt_16.value = 16
+dt_16.innerHTML = '16<Br>R$ '+ dt_16.value
+
+dt_17.value = 17
+dt_17.innerHTML = '17<Br>R$ '+ dt_17.value
+
+dt_18.value = 18
+dt_18.innerHTML = '18<Br>R$ '+ dt_18.value
+
+dt_19.value = 19
+dt_19.innerHTML = '19<Br>R$ '+ dt_19.value
+
+dt_20.value = 20
+dt_20.innerHTML = '20<Br>R$ ' + dt_20.value
+
+dt_21.value = 21
+dt_21.innerHTML = '21<Br>R$ '+ dt_21.value
+
+dt_22.value = 22
+dt_22.innerHTML = '22<Br>R$ '+ dt_22.value
+
+dt_23.value = 23
+dt_23.innerHTML = '23<Br>R$ '+ dt_23.value
+
+dt_24.value = 24
+dt_24.innerHTML = '24<Br>R$ '+ dt_24.value
+
+dt_25.value = 25
+dt_25.innerHTML = '25<Br>R$ '+ dt_25.value
+
+dt_26.value = 26
+dt_26.innerHTML = '26<Br>R$ '+ dt_26.value
+
+dt_27.value = 27
+dt_27.innerHTML = '27<Br>R$ '+ dt_27.value
+
+dt_28.value = 28
+dt_28.innerHTML = '28<Br>R$ '+ dt_28.value
+
+dt_29.value = 29
+dt_29.innerHTML = '29<Br>R$ '+ dt_29.value
+
+dt_30.value = 30
+dt_30.innerHTML = '30<Br>R$ '+ dt_30.value
+
+dt_31.value = 31
+dt_31.innerHTML = '31<Br>R$ '+ dt_31.value
+
+
+if (date_hj == d_1){
+    vl_tarifa.value = dt_1.value
+}else if(date_hj == d_2){
+    vl_tarifa.value = dt_2.value
+}else if(date_hj == d_3){
+    vl_tarifa.value = dt_3.value
+}else if(date_hj == d_4){
+    vl_tarifa.value = dt_4.value
+}else if(date_hj == d_5){
+    vl_tarifa.value = dt_5.value
+}else if(date_hj == d_6){
+    vl_tarifa.value = dt_6.value
+}else if(date_hj == d_7){
+    vl_tarifa.value = dt_7.value
+}else if(date_hj == d_8){
+    vl_tarifa.value = dt_8.value
+}else if(date_hj == d_9){
+    vl_tarifa.value = dt_9.value
+}else if(date_hj == d_10){
+    vl_tarifa.value = dt_10.value
+}else if(date_hj == d_11){
+    vl_tarifa.value = dt_11.value
+}else if(date_hj == d_12){
+    vl_tarifa.value = dt_12.value
+}else if(date_hj == d_13){
+    vl_tarifa.value = dt_13.value
+}else if(date_hj == d_14){
+    vl_tarifa.value = dt_14.value
+}else if(date_hj == d_15){
+    vl_tarifa.value = dt_15.value
+}else if(date_hj == d_16){
+    vl_tarifa.value = dt_16.value
+}else if(date_hj == d_17){
+    vl_tarifa.value = dt_17.value
+}else if(date_hj == d_18){
+    vl_tarifa.value = dt_18.value
+}else if(date_hj == d_19){
+    vl_tarifa.value = dt_19.value
+}else if(date_hj == d_20){
+    vl_tarifa.value = dt_20.value
+}else if(date_hj == d_21){
+    vl_tarifa.value = dt_21.value
+}else if(date_hj == d_22){
+    vl_tarifa.value = dt_22.value
+}else if(date_hj == d_23){
+    vl_tarifa.value = dt_23.value
+}else if(date_hj == d_24){
+    vl_tarifa.value = dt_24.value
+}else if(date_hj == d_25){
+    vl_tarifa.value = dt_25.value
+}else if(date_hj == d_26){
+    vl_tarifa.value = dt_26.value
+}else if(date_hj == d_27){
+    vl_tarifa.value = dt_27.value
+}else if(date_hj == d_28){
+    vl_tarifa.value = dt_28.value
+}else if(date_hj == d_29){
+    vl_tarifa.value = dt_29.value
+}else if(date_hj == d_30){
+    vl_tarifa.value = dt_30.value
+}else if(date_hj == d_31){
+    vl_tarifa.value = dt_31.value
+}
+
+console.log(d_21)
+
+console.log(date_hj)
+
+
+
+let P_1_S_C_TARIFA = document.getElementById('P_1_S_C_TARIFA')
+let P_1_S_C_CAFE = document.getElementById('P_1_S_C_CAFE')
+let P_1_S_C_SUBTOTAL = document.getElementById('P_1_S_C_SUBTOTAL')
+let P_1_S_C_ISS = document.getElementById('P_1_S_C_ISS')
+let P_1_S_C_TARIFA_TOTAL = document.getElementById('P_1_S_C_TARIFA_TOTAL')
+
+let P_2_S_C_TARIFA = document.getElementById('P_2_S_C_TARIFA')
+let P_2_S_C_CAFE = document.getElementById('P_2_S_C_CAFE')
+let P_2_S_C_SUBTOTAL = document.getElementById('P_2_S_C_SUBTOTAL')
+let P_2_S_C_ISS = document.getElementById('P_2_S_C_ISS')
+let P_2_S_C_TARIFA_TOTAL = document.getElementById('P_2_S_C_TARIFA_TOTAL')
+
+let P_3_S_C_TARIFA = document.getElementById('P_3_S_C_TARIFA')
+let P_3_S_C_CAFE = document.getElementById('P_3_S_C_CAFE')
+let P_3_S_C_SUBTOTAL = document.getElementById('P_3_S_C_SUBTOTAL')
+let P_3_S_C_ISS = document.getElementById('P_3_S_C_ISS')
+let P_3_S_C_TARIFA_TOTAL = document.getElementById('P_3_S_C_TARIFA_TOTAL')
+    
+P_1_S_C_TARIFA.value  = 0
+P_1_S_C_CAFE.value  = 0
+P_1_S_C_SUBTOTAL.value  = 0
+P_1_S_C_ISS.value = 0
+P_1_S_C_TARIFA_TOTAL.value  = 0
+
+P_2_S_C_TARIFA.value  = 0
+P_2_S_C_CAFE.value  = 0
+P_2_S_C_SUBTOTAL.value  = 0
+P_2_S_C_ISS.value = 0
+P_2_S_C_TARIFA_TOTAL.value  = 0
+
+P_3_S_C_TARIFA.value  = 0
+P_3_S_C_CAFE.value  = 0
+P_3_S_C_SUBTOTAL.value  = 0
+P_3_S_C_ISS.value = 0
+P_3_S_C_TARIFA_TOTAL.value  = 0
+
+////
+
+let P_1_C_C_TARIFA = document.getElementById('P_1_C_C_TARIFA')
+let P_1_C_C_CAFE = document.getElementById('P_1_C_C_CAFE')
+let P_1_C_C_SUBTOTAL = document.getElementById('P_1_C_C_SUBTOTAL')
+let P_1_C_C_ISS = document.getElementById('P_1_C_C_ISS')
+let P_1_C_C_TARIFA_TOTAL = document.getElementById('P_1_C_C_TARIFA_TOTAL')
+
+let P_2_C_C_TARIFA = document.getElementById('P_2_C_C_TARIFA')
+let P_2_C_C_CAFE = document.getElementById('P_2_C_C_CAFE')
+let P_2_C_C_SUBTOTAL = document.getElementById('P_2_C_C_SUBTOTAL')
+let P_2_C_C_ISS = document.getElementById('P_2_C_C_ISS')
+let P_2_C_C_TARIFA_TOTAL = document.getElementById('P_2_C_C_TARIFA_TOTAL')
+
+let P_3_C_C_TARIFA = document.getElementById('P_3_C_C_TARIFA')
+let P_3_C_C_CAFE = document.getElementById('P_3_C_C_CAFE')
+let P_3_C_C_SUBTOTAL = document.getElementById('P_3_C_C_SUBTOTAL')
+let P_3_C_C_ISS = document.getElementById('P_3_C_C_ISS')
+let P_3_C_C_TARIFA_TOTAL = document.getElementById('P_3_C_C_TARIFA_TOTAL')
+    
+let vl_cafe = 38
+let vl_cafe_s = 0
+
+P_1_C_C_TARIFA.value  = 0
+P_1_C_C_CAFE.value  = 0
+P_1_C_C_SUBTOTAL.value  = 0
+P_1_C_C_ISS.value = 0
+P_1_C_C_TARIFA_TOTAL.value  = 0
+
+P_2_C_C_TARIFA.value  = 0
+P_2_C_C_CAFE.value = 0
+P_2_C_C_SUBTOTAL.value  = 0
+P_2_C_C_ISS.value = 0
+P_2_C_C_TARIFA_TOTAL.value  = 0
+
+P_3_C_C_TARIFA.value  = 0
+P_3_C_C_CAFE.value  = 0
+P_3_C_C_SUBTOTAL.value  = 0
+P_3_C_C_ISS.value = 0
+P_3_C_C_TARIFA_TOTAL.value  = 0
+
+
+
+
+buscar.addEventListener("click",() => {
+
+    let TARIFA =  vl_tarifa.value
+
+
+    let CAFE_1 = vl_cafe_s
+    let SUBTOTAL_1 = parseFloat(TARIFA) + parseFloat(CAFE_1)
+    let ISS_1 =  parseFloat(SUBTOTAL_1 * 3 / 100)
+    let TARIFA_TOTAL_1 = parseFloat(SUBTOTAL_1) + parseFloat(ISS_1)
+
+    P_1_S_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_1_S_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_1).toFixed(2)
+    P_1_S_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_1).toFixed(2)
+    P_1_S_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_1).toFixed(2)
+    P_1_S_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_1).toFixed(2)
+
+    let CAFE_2 = vl_cafe_s
+    let SUBTOTAL_2 = parseFloat(TARIFA) + parseFloat(CAFE_2)
+    let ISS_2 =  parseFloat(SUBTOTAL_2 * 3 / 100)
+    let TARIFA_TOTAL_2 = parseFloat(SUBTOTAL_2) + parseFloat(ISS_2)
+
+    P_2_S_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_2_S_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_2).toFixed(2)
+    P_2_S_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_2).toFixed(2)
+    P_2_S_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_2).toFixed(2)
+    P_2_S_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_2).toFixed(2)
+
+    let CAFE_3 = vl_cafe_s
+    let SUBTOTAL_3 = parseFloat(TARIFA) + parseFloat(CAFE_3) + 30
+    let ISS_3 =  parseFloat(SUBTOTAL_3 * 3 / 100)
+    let TARIFA_TOTAL_3 = parseFloat(SUBTOTAL_3) + parseFloat(ISS_3)
+
+    P_3_S_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_3_S_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_3).toFixed(2)
+    P_3_S_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_3).toFixed(2)
+    P_3_S_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_3).toFixed(2)
+    P_3_S_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_3).toFixed(2)
+
+    /////
+
+    let CAFE_1C = vl_cafe
+    let SUBTOTAL_1C = parseFloat(TARIFA) + parseFloat(CAFE_1C)
+    let ISS_1C =  parseFloat(SUBTOTAL_1C * 3 / 100)
+    let TARIFA_TOTAL_1C = parseFloat(SUBTOTAL_1C) + parseFloat(ISS_1C)
+
+    P_1_C_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_1_C_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_1C).toFixed(2)
+    P_1_C_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_1C).toFixed(2)
+    P_1_C_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_1C).toFixed(2)
+    P_1_C_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_1C).toFixed(2)
+
+    let CAFE_2C = vl_cafe * 2
+    let SUBTOTAL_2C = parseFloat(TARIFA) + parseFloat(CAFE_2C)
+    let ISS_2C =  parseFloat(SUBTOTAL_2C * 3 / 100)
+    let TARIFA_TOTAL_2C = parseFloat(SUBTOTAL_2C) + parseFloat(ISS_2C)
+
+    P_2_C_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_2_C_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_2C).toFixed(2)
+    P_2_C_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_2C).toFixed(2)
+    P_2_C_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_2C).toFixed(2)
+    P_2_C_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_2C).toFixed(2)
+
+    let CAFE_3C = vl_cafe * 3
+    let SUBTOTAL_3C = parseFloat(TARIFA) + parseFloat(CAFE_3C) + 30
+    let ISS_3C =  parseFloat(SUBTOTAL_3C * 3 / 100)
+    let TARIFA_TOTAL_3C = parseFloat(SUBTOTAL_3C) + parseFloat(ISS_3C)
+
+    P_3_C_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_3_C_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_3C).toFixed(2)
+    P_3_C_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_3C).toFixed(2)
+    P_3_C_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_3C).toFixed(2)
+    P_3_C_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_3C).toFixed(2)
+
+})
+
+vl_tarifa.addEventListener("focus",() => {
+    vl_tarifa.value = ''
+})
+
+
+
+let TARIFA =  vl_tarifa.value
+
+
+    let CAFE_1 = vl_cafe_s
+    let SUBTOTAL_1 = parseFloat(TARIFA) + parseFloat(CAFE_1)
+    let ISS_1 =  parseFloat(SUBTOTAL_1 * 3 / 100)
+    let TARIFA_TOTAL_1 = parseFloat(SUBTOTAL_1) + parseFloat(ISS_1)
+
+    P_1_S_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_1_S_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_1).toFixed(2)
+    P_1_S_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_1).toFixed(2)
+    P_1_S_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_1).toFixed(2)
+    P_1_S_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_1).toFixed(2)
+
+    let CAFE_2 = vl_cafe_s
+    let SUBTOTAL_2 = parseFloat(TARIFA) + parseFloat(CAFE_2)
+    let ISS_2 =  parseFloat(SUBTOTAL_2 * 3 / 100)
+    let TARIFA_TOTAL_2 = parseFloat(SUBTOTAL_2) + parseFloat(ISS_2)
+
+    P_2_S_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_2_S_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_2).toFixed(2)
+    P_2_S_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_2).toFixed(2)
+    P_2_S_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_2).toFixed(2)
+    P_2_S_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_2).toFixed(2)
+
+    let CAFE_3 = vl_cafe_s
+    let SUBTOTAL_3 = parseFloat(TARIFA) + parseFloat(CAFE_3) + 30
+    let ISS_3 =  parseFloat(SUBTOTAL_3 * 3 / 100)
+    let TARIFA_TOTAL_3 = parseFloat(SUBTOTAL_3) + parseFloat(ISS_3)
+
+    P_3_S_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_3_S_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_3).toFixed(2)
+    P_3_S_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_3).toFixed(2)
+    P_3_S_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_3).toFixed(2)
+    P_3_S_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_3).toFixed(2)
+
+    /////
+
+    let CAFE_1C = vl_cafe
+    let SUBTOTAL_1C = parseFloat(TARIFA) + parseFloat(CAFE_1C)
+    let ISS_1C =  parseFloat(SUBTOTAL_1C * 3 / 100)
+    let TARIFA_TOTAL_1C = parseFloat(SUBTOTAL_1C) + parseFloat(ISS_1C)
+
+    P_1_C_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_1_C_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_1C).toFixed(2)
+    P_1_C_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_1C).toFixed(2)
+    P_1_C_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_1C).toFixed(2)
+    P_1_C_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_1C).toFixed(2)
+
+    let CAFE_2C = vl_cafe * 2
+    let SUBTOTAL_2C = parseFloat(TARIFA) + parseFloat(CAFE_2C)
+    let ISS_2C =  parseFloat(SUBTOTAL_2C * 3 / 100)
+    let TARIFA_TOTAL_2C = parseFloat(SUBTOTAL_2C) + parseFloat(ISS_2C)
+
+    P_2_C_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_2_C_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_2C).toFixed(2)
+    P_2_C_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_2C).toFixed(2)
+    P_2_C_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_2C).toFixed(2)
+    P_2_C_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_2C).toFixed(2)
+
+    let CAFE_3C = vl_cafe * 3
+    let SUBTOTAL_3C = parseFloat(TARIFA) + parseFloat(CAFE_3C) + 30
+    let ISS_3C =  parseFloat(SUBTOTAL_3C * 3 / 100)
+    let TARIFA_TOTAL_3C = parseFloat(SUBTOTAL_3C) + parseFloat(ISS_3C)
+
+    P_3_C_C_TARIFA.innerHTML = 'R$ ' + parseFloat(TARIFA).toFixed(2)
+    P_3_C_C_CAFE.innerHTML = 'R$ ' + parseFloat(CAFE_3C).toFixed(2)
+    P_3_C_C_SUBTOTAL.innerHTML = 'R$ ' + parseFloat(SUBTOTAL_3C).toFixed(2)
+    P_3_C_C_ISS.innerHTML = 'R$ ' + parseFloat(ISS_3C).toFixed(2)
+    P_3_C_C_TARIFA_TOTAL.innerHTML = 'R$ ' + parseFloat(TARIFA_TOTAL_3C).toFixed(2)
